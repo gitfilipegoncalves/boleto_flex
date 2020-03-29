d6fab98ca95bea1987d3a18cd7121e2e3f18759e
Filipe <filipegoncalves.ti@gmail.com>
2020-03-29 10:51:25 -0300

    Finalizando a migração, evoluindo com o model de cliente e adicionando seus recursos com Flask-Restful

--- app/restapi/models.py
+++ app/restapi/models.py
@@ -5,8 +5,35 @@ class CustomerModel(db.Model):
 
     __tablename__ = 'customers'
 
-    id = db.Column(db.String, primary_key=True)
+    customer_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     email = db.Column(db.String(80))
     phone = db.Column(db.String(9))
     doc = db.Column(db.String(11))
+
+    def __init__(self, customer_id, name, email, phone, doc):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.doc = doc
+
+    def json(self):
+        return {
+            'customer_id': self.customer_id,
+            'name': self.name,
+            'email': self.email,
+            'phone': self.phone,
+            'doc': self.doc
+        }
+
+    @classmethod
+    def find_customer(cls, customer_id):
+        customer = cls.query.filter_by(customer_id=customer_id).first()
+        if customer:
+            return customer
+        return None
+
+    def save_customer(self):
+        db.session.add(self)
+        db.session.commit()
--- app/restapi/resources.py
+++ app/restapi/resources.py
@@ -1,31 +1,35 @@
-from flask_restful import Resource
-
-customers = [
-    {
-        'name': 'Filipe',
-        'email': 'filipe@email.com',
-        'phone': '991264747',
-        'doc': '04082684955',
-    },
-    {
-        'name': 'Luis',
-        'email': 'luis@email.com',
-        'phone': '233456788',
-        'doc': '04061804566',
-    },
-    {
-        'name': 'Livia',
-        'email': 'livia@email.com',
-        'phone': '899876353',
-        'doc': '04084971833',
-    }
-]
+from flask_restful import Resource, reqparse
+from .models import CustomerModel
 
 
 class CustomersResource(Resource):
     def get(self):
-        return {'customers': customers}
+        return {'customers': [customers.json() for customers in
+                              CustomerModel.query.all()]}
 
 
 class CustomersItemResource(Resource):
-    pass
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('name', type=str, required=True,
+                           help="Campo 'nome' não pode ser deixado em branco")
+    atributos.add_argument('email', type=str, required=True,
+                           help="Campo 'email' não pode ser deixado em branco")
+    atributos.add_argument('phone')
+    atributos.add_argument('doc')
+
+    def get(self, customer_id):
+        customer = CustomerModel.find_customer(customer_id)
+        if customer:
+            return customer.json()
+        return {'message': 'Cliente não existe!'}
+
+    def post(self, customer_id):
+        if CustomerModel.find_customer(customer_id):
+            return {"message": "Cliente com id '{}' já existe!".format(customer_id)}, 400
+        data = CustomersItemResource.atributos.parse_args()
+        customer = CustomerModel(customer_id, **data)
+        try:
+            customer.save_customer()
+        except:
+            return {'message': 'Erro ao tentar salvar o cliente'}, 500
+        return customer.json()
